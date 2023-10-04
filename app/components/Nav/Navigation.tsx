f22d56343368b450d40aacfac06e5981c9512774
michalrutz <46682195+michalrutz@users.noreply.github.com>
2023-10-04 13:40:29 +0200

    Update Navigation.tsx options

--- app/components/Nav/Navigation.tsx
+++ app/components/Nav/Navigation.tsx
@@ -1,23 +1,20 @@
 import Link from "next/link";
 import CardButton from "./CardButton";
 import { getServerSession } from 'next-auth/next'
-import { Options } from "../../api/auth/[...nextauth]/route";
+import { options } from "../../api/auth/[...nextauth]/route";
 import HoverMenu from "./HoverMenu";
 import MaxWrapper from "../MaxWrapper";
 
 
 export default async function Navigation() {
-  const session = await getServerSession(Options)
+  const session = await getServerSession(options)
 
   return (
     <MaxWrapper>
-
       <div className="bg-transparent flex flex-row justify-between items-center pt-1 pb-1 text-slate-600 w-full border-b text-lg">
-      
         <div className="flex flex-row items-center  border rounded-full p-1.5 pl-3 pr-3 shade">
           <Link href="/">Gallery</Link>
         </div>
-
         <div className="flex flex-row gap-3 justify-between items-center border pl-1 pr-3 rounded-full shade">
           { session && session.user ?
           <HoverMenu user={session.user}/> : 
@@ -27,6 +24,5 @@ export default async function Navigation() {
         </div>
       </div>
       </MaxWrapper>
-
   );
 }
